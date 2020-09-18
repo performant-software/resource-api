1402776647cbc3dac5cf1084d30f087a7f4c04db
Derek Leadbetter <dleadbetter55@gmail.com>
2020-09-18 09:41:19 -0400

    Updating engine.rb to require dependencies

--- lib/resource_api/engine.rb
+++ lib/resource_api/engine.rb
@@ -1,3 +1,9 @@
+Gem.loaded_specs['resource_api'].dependencies.each do |d|
+  require d.name unless d.type == :development
+end
+
+require 'resource_api/engine'
+
 module ResourceApi
   class Engine < ::Rails::Engine
   end
