f5019f1fe9f24f9b34d54261bd9c3a8c62392424
Derek Leadbetter <dleadbetter55@gmail.com>
2020-11-30 16:33:07 -0500

    CC #158 - Updating searchable controller concern to treat symbols and strings differently to allow for custom SQL formatting

--- app/controllers/concerns/api/searchable.rb
+++ app/controllers/concerns/api/searchable.rb
@@ -4,9 +4,22 @@ module Api::Searchable
   included do
     def self.search_attributes(*attrs)
       @attrs ||= []
-      attrs.each do |attr|
-        @attrs << "#{self.controller_name}.#{attr.to_s}" if attrs.present?
+
+      # Iterate over the attributes and add them for the list.
+      #
+      # For symbols, we'll assume that the column is on the primary table and use the controller name to format the SQL
+      # as <table-name>.<column_name>.
+      #
+      # For strings, we'll assume that the column is NOT on the primary table (instead possibly a join table) and
+      # allow the controller to format the SQL as appropriate.
+      attrs&.each do |attr|
+        if attr.is_a?(Symbol)
+          @attrs << "#{self.controller_name}.#{attr.to_s}"
+        elsif attr.is_a?(String)
+          @attrs << attr
+        end
       end
+
       @attrs
     end
 
