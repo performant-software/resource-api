e7a66f1f9cfc9b7fe88762c9269a423d148fed48
Derek Leadbetter <dleadbetter55@gmail.com>
2021-07-23 08:32:55 -0400

    BASIRA #88 - Updating Resourceable concern to default boolean columns to "false" before saving

--- app/models/concerns/resourceable.rb
+++ app/models/concerns/resourceable.rb
@@ -1,6 +1,24 @@
 module Resourceable
   extend ActiveSupport::Concern
 
+  included do
+    before_create :set_booleans
+
+    private
+
+    # Default any boolean columns to "false" if a value is not provided
+    def set_booleans
+      boolean_attributes = self
+                             .class.columns_hash
+                             .select{ |k,v| v.type == :boolean }
+                             .map{ |k, v| k }
+
+      boolean_attributes.each do |attribute|
+        self[attribute.to_sym] = false unless self[attribute.to_sym].present?
+      end
+    end
+  end
+
   class << self
     def included(base)
       base.extend ClassMethods
